cb418400ff820485f8cfecad6bc46d0ae0bdf31c
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-12-05 09:31:42 +0100

    #9, Fixes #10 - Création du footer component + ajout du nav component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-[#3f3f3f] text-white py-4">
+    <footer class="container mx-auto text-center">
+        <p class="text-sm">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+        <p class="text-sm mt-2" style="color: #4467C4;">&copy; {{ date('Y') }} Your Company. All rights reserved.</p>
+    </footer>
+</div>
