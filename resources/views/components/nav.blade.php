0cea0acd8946c9e684cf8dae41e244946bf6131f
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-12-05 09:32:52 +0100

    Fixes #9 - création du nav component

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
-</div>
+<nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="text-white text-lg font-semibold">
+            Boargame App
+        </div>
+        <ul class="flex space-x-4">
+            <li><a href="#" class="text-gray-300 hover:text-white transition duration-300">Home</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white transition duration-300">About</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white transition duration-300">Services</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white transition duration-300">Contact</a></li>
+        </ul>
+    </div>
+</nav>
